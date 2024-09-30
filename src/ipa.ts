91ddb142b2f9c2b0d9d43c463f33f124775930c9
robo-mop <realaryanpingle@gmail.com>
2024-09-30 23:10:53 +0530

    Improve IPA translation logic

--- src/ipa.ts
+++ src/ipa.ts
@@ -19,36 +19,66 @@ export function translateWord(word: string): string | null {
 /**
  * Convert a sentence from English to Phonetic.
  */
-export function translateSentence(sentence: string): string | null {
-    // Match all sequences of letters, translate them if possible
-
+export function translateSentence(sentence: string): string {
+    // Regex that matches characters that could be part of an IPA word
+    const wordCharRegex = /[\p{Letter}']/u;
     let englishEscape = false;
-    const punctuationRegex = /[^\s\n\.,?!\-]+/gu;
-    return sentence.replace(punctuationRegex, (word) => {
-        const startsWithAt = word.startsWith("@");
-        const endsWithAt = word.endsWith("@");
+    let result = "";
+    let buffer = "";
 
-        if (startsWithAt && endsWithAt) {
-            // Start and end english sequence
-            return word;
-        } else if (startsWithAt) {
-            // Start english sequence
-            englishEscape = true;
-            return word;
-        } else if (endsWithAt) {
-            // End english sequence
-            englishEscape = false;
-            return word;
-        } else if (englishEscape) {
-            // Continue english sequence
-            return word;
-        }
-
-        const lowerCaseWord = word.toLowerCase();
-        if (lowerCaseWord in IPADict) {
-            return translateWord(lowerCaseWord);
+    // Utilities
+    function translateAndDumpBuffer() {
+        const lowerCaseBuffer = buffer.toLowerCase();
+        console.log("Tryna translate ", lowerCaseBuffer);
+        if (lowerCaseBuffer in IPADict) {
+            // Add the translated word to the result
+            const translation = IPADict[lowerCaseBuffer];
+            result += translation;
+            buffer = "";
         } else {
-            return "";
+            // Ignore this word
+            buffer = "";
         }
-    });
+    }
+
+    // Parse each character of the English input
+    for (let i = 0; i < sentence.length; ++i) {
+        const c = sentence.charAt(i);
+
+        // If this is part of an english-escaped sequence,
+        // just add it bro.
+        if (englishEscape === true) {
+            result += c;
+            if (c === "@") {
+                englishEscape = false;
+            }
+            continue;
+        }
+
+        // Start of an english-escaped sequence
+        if (c === "@") {
+            translateAndDumpBuffer();
+            result += c;
+            englishEscape = true;
+            continue;
+        }
+
+        // Punctuation
+        if (!wordCharRegex.test(c)) {
+            translateAndDumpBuffer();
+            result += c;
+            continue;
+        }
+
+        // Whatever it is, must be a character of a valid word
+        // (not necessarily translatable)
+        buffer += c;
+
+        // If this is the last character
+        if (i === sentence.length - 1) {
+            translateAndDumpBuffer();
+        }
+    }
+
+    return result;
 }
